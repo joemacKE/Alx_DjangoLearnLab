fc81179fcf5455b8085267a6225aa8251e9276e1
joemacKE <josephmuga2020@gmail.com>
2025-07-09 14:46:04 +0300

    Message

--- Introduction_to_Django/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6z8p!2pmb%07ay-j1x2utig!!9_anmh7q=xq*1p7jf7!nnzr8w'
+SECRET_KEY = 'django-insecure-#9he+vsy3t1!8ufp0x$zfeeltv&95)hr2*$l&+5ly2*uz$+q0z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
