8f49b249d35243d9900826d8c0858ddde67a0bed
joemacKE <josephmuga2020@gmail.com>
2025-07-18 13:44:36 +0300

    made changes.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic import ListView, DetailView, TemplateView
 from .models import Book, Library, Librarian, Author
 
@@ -7,7 +6,7 @@ from .models import Book, Library, Librarian, Author
 def book_list(request):
     books = Book.objects.all()
     context = {'list_books': books}
-    return render(request, 'books/list_books.html', context)
+    return render(request, 'relationship_app/list_books.html', context)
 
 class BookDetailView(DetailView):
     model = Library
