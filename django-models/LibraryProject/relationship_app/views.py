6b0c13338c25ec8f01d9befa756376b175a202d2
joemacKE <josephmuga2020@gmail.com>
2025-07-18 13:54:05 +0300

    made changes.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -10,7 +10,7 @@ def book_list(request):
 
 class BookDetailView(DetailView):
     model = Library
-    template_name = 'books/library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
