16b8301bb36d93da7e7fe881b1d6dee723f14b62
joemacKE <josephmuga2020@gmail.com>
2025-07-18 13:27:28 +0300

    made changes.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView, TemplateView
-from .models import Book, Library, Librarian, Author
+from .models import Library
+from .models import Book
 
 
 def book_list(request):
@@ -21,4 +22,4 @@ class BookDetailView(DetailView):
 
 # Create your views here.
 #LibraryProject/relationship_app/views.py 
-# doesn't contain: ["relationship_app/list_books.html"]
+# doesn't contain: ["relationship_app/library_detail.html", "from .models import Library"]
