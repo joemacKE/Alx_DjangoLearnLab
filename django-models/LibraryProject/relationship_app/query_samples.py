33b037650b71a8bd4ab6fc8ce931ba15713d994c
joemacKE <josephmuga2020@gmail.com>
2025-07-18 11:57:24 +0300

    Added the django-models.

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,5 +1,7 @@
 from .models import Author, Book, Library, Librarian
 
+books = Book.objects.all()
+
 books = Book.objects.prefetch_related(author='name')
 
 librarian = Librarian.objects.get(name='library_name')
