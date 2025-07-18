59efa27f81760ce4df2ff8b1ab1db500f148724e
joemacKE <josephmuga2020@gmail.com>
2025-07-18 14:23:21 +0300

    made changes.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,14 @@
 from .views import views, list_books, LibraryDetailView
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns = [
     path('list_books/', views.list_books.as_view(), name='list_books' ),
-    path('login/', views.login.as_view(), name='login'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('register/', views.register.as_view(), name='register'),
-    path('logout/', views.logout.as_view(), name='logout' ),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout' ),
 ]
 
 #LibraryProject/relationship_app/urls.py 
-# doesn't contain: ["from .views import list_books", "LibraryDetailView"]
+# doesn't contain: ["LogoutView.as_view(template_name=", "LoginView.as_view(template_name="]
