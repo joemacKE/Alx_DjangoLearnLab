afa1ac75fb880f8c7e14ef51d2866da7c04a0d7d
joemacKE <josephmuga2020@gmail.com>
2025-07-19 14:05:56 +0300

    made changes.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,46 +1,25 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponseForbidden, HttpResponse
+from django.shortcuts import render
 from django.views.generic import ListView, DetailView, TemplateView, CreateView
-from .models import Librarian, Author
+from .models import Book, Library
 from django.contrib.auth import login
-from .forms import LibrarianProfileForm, MemberProfileForm
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required, user_passes_test
 
-
-#checks if user is admin
 def is_admin(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'admin'
 
-#checks if user is librarian
 def is_librarian(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'librarian'
 
-#checks if user is member
 def is_member(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'member'
 
-#the admin view
+# --- Views ---
 @login_required
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
-
-
-#librarian view
-@login_required
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'relationship_app/librarian_view.html')
-
-
-#member view
-@login_required
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, 'relationship_app/member_view')
-
 
 
 
