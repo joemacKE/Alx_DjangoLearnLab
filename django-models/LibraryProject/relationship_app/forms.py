1a5f815a0f3946dec364d79ed826a55487b75961
joemacKE <josephmuga2020@gmail.com>
2025-07-19 13:19:02 +0300

    made changes.

---
+++ django-models/LibraryProject/relationship_app/forms.py
@@ -0,0 +1,19 @@
+from .models import UserProfile, Author, Book, Library, Librarian
+from django import forms
+from .admin import admin
+
+class AdminProfileForm(forms.ModelForm):
+    class Meta:
+        model = admin
+        fields = ['username', 'email']
+
+class LibrarianProfileForm(forms.ModelForm):
+    class Meta:
+        model = Librarian
+        fields = ['name', 'library']
+
+class MemberProfileForm(forms.ModelForm):
+    class Meta:
+        model = Author
+        fields = ['name']
+    
