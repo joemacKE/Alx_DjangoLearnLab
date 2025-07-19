effb5b7b5bede3ed4d1ded90eeab2926a167cd11
joemacKE <josephmuga2020@gmail.com>
2025-07-19 13:17:39 +0300

    made changes.

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+ROLE_CHOICES = [
+    ('admin', 'Admin'),
+    ('librarian', 'Librarian'),
+    ('member', 'Member')
+]
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=120, choices=ROLE_CHOICES)
+
 class Author(models.Model):
-    ROLE_CHOICES = [
-        ('admin', 'Admin'),
-        ('librarian', 'Librarian'),
-        ('member', 'Member')
-    ]
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
 
     def __str__(self):
