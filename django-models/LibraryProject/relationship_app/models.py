3f1f30f58dc372d0e46e69f66ad0b86ba1cc8f0e
joemacKE <josephmuga2020@gmail.com>
2025-07-19 13:56:36 +0300

    made changes.

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -12,6 +12,10 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=120, choices=ROLE_CHOICES)
 
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
 class Author(models.Model):
     name = models.CharField(max_length=200)
 
