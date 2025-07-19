c52230367648a8c893ec0890317ac6d3f2c837d0
joemacKE <josephmuga2020@gmail.com>
2025-07-19 14:11:47 +0300

    made changes.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,12 +9,6 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 def is_admin(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'admin'
 
-def is_librarian(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'librarian'
-
-def is_member(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'member'
-
 # --- Views ---
 @login_required
 @user_passes_test(is_admin)
