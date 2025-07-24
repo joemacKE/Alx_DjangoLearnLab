2706685822342acfca1d051f7128fa6fe05347e1
joemacKE <josephmuga2020@gmail.com>
2025-07-24 15:10:20 +0300

    Implemented the logic of creating a user and superuser.

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from .models import Book, CustomUser, CustomUserManager
+from .models import CustomUser, CustomUserManager
 
+admin.site.register(CustomUser, CustomUserManager)
 admin.site.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     list_filter = ('author', 'publication_year')
     search_fields = ('title', 'author')
 
-# Register your models here.
-admin.site.register(CustomUser, CustomUserManager)
+
+
+#oesn't contain: ["admin.site.register(CustomUser, CustomUserAdmin)"]
