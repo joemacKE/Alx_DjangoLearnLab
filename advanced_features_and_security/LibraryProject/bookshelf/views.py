415588f38e85281f2eaa888e10d9b4c98c7c659c
joemacKE <josephmuga2020@gmail.com>
2025-07-24 15:34:29 +0300

    implemented the first requirement.

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from relationship_app.forms import BookForm
+from django.contrib.auth.decorators import permission_required
+
+@permission_required('bookshelf.permissions', raise_exception=True)
+def can_view(request):
+    
+
+
 
 # Create your views here.
