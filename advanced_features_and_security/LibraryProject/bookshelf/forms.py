44d79fd7924c04b793cd3245dfe96c67d05bb207
joemacKE <josephmuga2020@gmail.com>
2025-07-25 14:16:35 +0300

    Created forms.py.

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,4 @@
+from .models import Book
+#Missing: LibraryProject/bookshelf/forms.py
+#  - LibraryProject/bookshelf/forms.py doesn't 
+# exist
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand
+
 from django.shortcuts import render, redirect
 from relationship_app.forms import BookForm
 from .models import Book
@@ -18,31 +18,6 @@ def can_view(request):
     return render(request, 'bookshelf/book_list.html', {'form':form})
 
 
-class Command(BaseCommand):
-    help = 'Create default groups and assign permissionss'
-
-    def handle(self, *args, **kwargs):
-        #editor group
-        editor_group, _ = Group.objects.get_or_create(name="Editor")
-        permissions = Permission.objects.filter(content_type=ContentType.objects.get_for_model(Book), codename_in=[
-            'can_edit', 'can_create'
-
-        ])
-        editor_group.permissions.set(permissions)
-
-        #viewers group
-        viewer_group, _ = Group.objects.get_or_create(name='Viewer')
-        permissions = Permission.objects.filter(content_type=ContentType.objects.get_for_model(Book), codename_in=[
-            'can_view'
-        ])
-        viewer_group.permissions.set(permissions)
-
-        #admin view group
-        admin_group, _ = Group.objects.get_or_create(name='Admin')
-        permissions = Permission.objects.filter(Content_type=ContentType.objects.get_for_model(Book), codename_in=[
-            'can_add', 'can_view', 'can_create', 'can_delete'
-        ])
-        admin_group.permissions.set(permissions)
 
 #@permission_required('bookshelf.permission', raise_exception=True)
 
