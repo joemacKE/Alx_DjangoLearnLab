7bb7decfd62c373bc32280f9050f5848b097058b
joemacKE <josephmuga2020@gmail.com>
2025-07-25 14:07:39 +0300

    Implemented the test.

--- advanced_features_and_security/LibraryProject/bookshelf/tests.py
+++ advanced_features_and_security/LibraryProject/bookshelf/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth.models import User, Group
+
+# Create test users
+editor_user = User.objects.create_user(username='editor1', password='testpass123')
+viewer_user = User.objects.create_user(username='viewer1', password='testpass123')
+admin_user = User.objects.create_user(username='admin1', password='testpass123')
+
+# Assign to groups
+editor_group = Group.objects.get(name='Editor')
+viewer_group = Group.objects.get(name='Viewer')
+admin_group = Group.objects.get(name='Admin')
+
+editor_user.groups.add(editor_group)
+viewer_user.groups.add(viewer_group)
+admin_user.groups.add(admin_group)
+
 
 # Create your tests here.
