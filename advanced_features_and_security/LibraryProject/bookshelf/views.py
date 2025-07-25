cf1d6b8ed4d6a812eb2164d002354c36daa2de2e
joemacKE <josephmuga2020@gmail.com>
2025-07-25 14:23:03 +0300

    created the views.py.

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,7 @@
 
 from django.shortcuts import render, redirect
 from relationship_app.forms import BookForm
-from .models import Book
+from .forms import ExampleForm
 from django.contrib.auth.decorators import permission_required
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.models import Group, Permission
@@ -19,9 +19,5 @@ def can_view(request):
 
 
 
-#@permission_required('bookshelf.permission', raise_exception=True)
-
-
-
-# Create your views here.
-#LibraryProject/bookshelf/views.py doesn't contain: ["book_list"]
+# - LibraryProject/bookshelf/views.py doesn't contain: 
+# ["from .forms import ExampleForm"]
