4147fb5ccaa7a95b89bf10e0fa82c4062a968950
joemacKE <josephmuga2020@gmail.com>
2025-07-25 14:01:36 +0300

    implemented the groups and permissions.

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,10 @@
+from django.core.management.base import BaseCommand
 from django.shortcuts import render, redirect
 from relationship_app.forms import BookForm
+from .models import Book
 from django.contrib.auth.decorators import permission_required
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import Group, Permission
 
 @permission_required('bookshelf.permissions', raise_exception=True)
 def can_view(request):
@@ -13,7 +17,34 @@ def can_view(request):
         form = BookForm()
     return render(request, 'bookshelf/book_list.html', {'form':form})
 
-@permission_required('bookshelf.permission', raise_exception=True)
+
+class Command(BaseCommand):
+    help = 'Create default groups and assign permissionss'
+
+    def handle(self, *args, **kwargs):
+        #editor group
+        editor_group, _ = Group.objects.get_or_create(name="Editor")
+        permissions = Permission.objects.filter(content_type=ContentType.objects.get_for_model(Book), codename_in=[
+            'can_edit', 'can_create'
+
+        ])
+        editor_group.permissions.set(permissions)
+
+        #viewers group
+        viewer_group, _ = Group.objects.get_or_create(name='Viewer')
+        permissions = Permission.objects.filter(content_type=ContentType.objects.get_for_model(Book), codename_in=[
+            'can_view'
+        ])
+        viewer_group.permissions.set(permissions)
+
+        #admin view group
+        admin_group, _ = Group.objects.get_or_create(name='Admin')
+        permissions = Permission.objects.filter(Content_type=ContentType.objects.get_for_model(Book), codename_in=[
+            'can_add', 'can_view', 'can_create', 'can_delete'
+        ])
+        admin_group.permissions.set(permissions)
+
+#@permission_required('bookshelf.permission', raise_exception=True)
 
 
 
