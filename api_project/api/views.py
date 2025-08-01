7b0d3bb210a749d0f02c534e3085222c9710e4ed
joemacKE <josephmuga2020@gmail.com>
2025-08-01 17:19:55 +0300

    implemented the views.py file.

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .serializers import BookSerializer
+from rest_framework import generics
+from .models import Book
+
+
+class BookList(generics.ListAPIView):
+    query_set = Book.objects.all()
+    serializer_class = BookSerializer
+
 
 # Create your views here.
+#api/views.py doesn't contain: 
+# ["BookList", "generics.ListAPIView", 
+# "from .serializers import BookSerializer"]
