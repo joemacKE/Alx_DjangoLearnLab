da70377aa7b2d88adf26d92f40954414ac3e7fe1
joemacKE <josephmuga2020@gmail.com>
2025-08-02 13:15:25 +0300

    Implemented the ViewSet which inherits from viewsets.ModelViewSet to handle all the CRUDE operations.

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .serializers import BookSerializer
+from rest_framework.response import Response
 from rest_framework import generics
 from .models import Book
+from rest_framework import viewsets
 
 
 class BookList(generics.ListAPIView):
     query_set = Book.objects.all()
     serializer_class = BookSerializer
 
-
-# Create your views here.
-#api/views.py doesn't contain: 
-# ["BookList", "generics.ListAPIView", 
-# "from .serializers import BookSerializer"]
+class BookViewSet(viewsets.ModelViewSet):
+    def list(self, request):
+        queryset = Book.objects.all()
+        serializer = BookSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    def retrieve(self, request, pk):
+        queryset = Book.objects.all()
+        book = get_object_or_404(queryset, pk=pk)
+        serializer = BookSerializer(book)
+        return Response(serializer.data)
