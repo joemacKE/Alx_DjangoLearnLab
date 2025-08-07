8f3df76026b0bdb1cea6a181655c89852378b854
joemacKE <josephmuga2020@gmail.com>
2025-08-07 13:06:33 +0300

    Implemented the BookSerializer.

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,7 +1,15 @@
 from .models import (Book, Author)
 from .serializers import serializers
+from datetime import timezone
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
         fields = "__all__"
+
+
+class BookSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    publication_year = serializers.IntegerField()
+
+
