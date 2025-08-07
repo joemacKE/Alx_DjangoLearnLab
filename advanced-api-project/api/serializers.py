d3bffd6f9aa69254da307640eca8651e724e88be
joemacKE <josephmuga2020@gmail.com>
2025-08-07 13:15:22 +0300

    Changed the position of AuthorSerializer in order to reference the BookSerializer and create the relationship.

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -2,20 +2,23 @@ from .models import (Book, Author)
 from .serializers import serializers
 from datetime import timezone
 
+
+
+
+class BookSerializer(serializers.ModelSerializer):
+
+    title = serializers.CharField(max_length=255)
+    publication_year = serializers.IntegerField()
+
+
+    def get_publication_year(self):
+        ...
+
 class AuthorSerializer(serializers.ModelSerializer):
+    book = BookSerializer(read_only=True, many=True)
     class Meta:
         model = Author
         fields = "__all__"
 
 
-class BookSerializer(serializers.ModelSerializer):
-    title = serializers.CharField(max_length=255)
-    publication_year = serializers.IntegerField()
-
-    def get_publication_year(self):
-        ...
-
-
-
-#api/serializers.py doesn't contain: 
-# ["class BookSerializer(serializers.ModelSerializer)", "serializers.ValidationError"]
+#api/serializers.py doesn't contain: ["(many=True, read_only=True)"]
