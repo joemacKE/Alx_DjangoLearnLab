6a59bc659704248ba9ff2cf30c1d9d409cc74b92
joemacKE <josephmuga2020@gmail.com>
2025-08-07 13:55:50 +0300

    Made some changes.

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,12 +2,24 @@ from rest_framework import generics
 from api.serializers import BookSerializer, AuthorSerializer
 from api.models import Book, Author
 
-class BookListView(generics.ListCreateAPIView):
+class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 
-class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
