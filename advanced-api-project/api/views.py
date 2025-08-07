070aa0463c235a9c209a8f1965d91941ef7cb151
joemacKE <josephmuga2020@gmail.com>
2025-08-07 13:36:03 +0300

    Implemented the DetailView .

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,10 +2,16 @@ from rest_framework import generics
 from api.serializers import BookSerializer, AuthorSerializer
 from api.models import Book, Author
 
-class ListView(generics.ListCreateAPIView):
+class ListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class DetailView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 
 
-# Create your views here.
+# api/views.py doesn't contain:
+#  ["DetailView", "CreateView", "UpdateView", "DeleteView"]
