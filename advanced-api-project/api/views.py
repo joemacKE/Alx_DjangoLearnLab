736c93c9f322ff75afa59ab6ba3fafb888bc4666
joemacKE <josephmuga2020@gmail.com>
2025-08-07 13:34:00 +0300

    Implemented the ListView in the views.py file.

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from api.serializers import BookSerializer, AuthorSerializer
+from api.models import Book, Author
+
+class ListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
 
 # Create your views here.
