830c3a892ae3d2dc545a9a4d5b235aca89798b31
joemacKE <josephmuga2020@gmail.com>
2025-08-12 16:57:54 +0300

    Implemented some validations.

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -20,14 +20,32 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email']
+    
+    def clean_data(self):
+        email = self.cleaned_data.get('email')
+        if not email:
+            raise forms.ValidationError("Email field cannot be empty")
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email is already in use")
+        return email
 
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['bio', 'profile_picture']
+    
+
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+    
+    def clean_content(self):
+        content = self.cleaned_data.get('content')
+        if not content:
+            raise forms.ValidationError("Content cannot be empty")
+        if len(content) < 5:
+            raise forms.ValidationError("Content must be at least 5 characters long")
+        return content
