1cdecb63c3844b7d62ccf4b694a68d15d50b376c
joemacKE <josephmuga2020@gmail.com>
2025-08-12 14:07:38 +0300

    Added the implementation in the urls.py.

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name = 'login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile.profile, name='profile'),
 ]
