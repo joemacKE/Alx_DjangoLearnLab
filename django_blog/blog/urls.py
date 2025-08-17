e170d7762f643c8ed20c5221d8742fb217c947b9
joemacKE <josephmuga2020@gmail.com>
2025-08-17 10:52:28 +0300

    Missing forward slash threw an error in the blogs.urls file.

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name = 'post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name = 'post-delete'),
     path('post/new/', PostCreateView.as_view(), name = 'post-create'),
-    path('comment<int:pk>/update/', CommentUpdateView.as_view(), name = 'update-comment'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name = 'update-comment'),
     path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name = "post-comment"),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete-comment')
 
