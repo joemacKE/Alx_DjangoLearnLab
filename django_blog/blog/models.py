ea07a7f1fd376e7dccfc6ef0d2b41c830e07820d
joemacKE <josephmuga2020@gmail.com>
2025-08-17 10:59:36 +0300

    Added the Tag model in our models file and established the relationship between Tag and Post.

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Tag(models.Model):
+    name = models.CharField(max_length=120)
 
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag, on_delete = models.CASCADE)
 
     def __str__(self):
         return f"{self.title} by {self.author} published {self.published_date}"
@@ -29,6 +34,9 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author}"
+
+
+
 
 
 # Create your models here.
