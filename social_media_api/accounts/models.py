35872d4dee51f82a85fc29e6babdafa825feeeb0
joemacKE <josephmuga2020@gmail.com>
2025-08-20 10:45:26 +0300

    Created the CustomUserManager that the CustomUser will inherit from.

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser, User
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("User must provide a valid email address")
+        extra_fields.setdefault('username', email)
+
+        user = self.model(
+            email = self.normalize_email(email), **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("You must provide a valid email")
+        user = self.create_user(
+            email = email,
+            password = password
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+
+
+class CustomUser(AbstractUser):
+    bio = models.TextField()
+    profile_picture = models.ImageField(upload_to='profile/', blank=True)
+    follower = models.ManyToManyField(User, on_delete=models.CASCADE, symmetrical=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.profile_picture
+
 
 # Create your models here.
