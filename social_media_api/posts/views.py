1ff05c29348396dd7f3e3bd8f2a8dc74459c4bfb
joemacKE <josephmuga2020@gmail.com>
2025-08-20 13:22:26 +0300

    Realized i should added more authentication for instance TokenAuthentication.

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,21 +1,21 @@
 from posts.models import Post, Comment
 from posts.serializers import PostSerializer, CommentSerializer
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, BasePermission
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 from rest_framework.response import Response
 from rest_framework import status, viewsets
 
 
 class PostListViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
-    serializer_calsses = PostSerializer
-    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    serializer_calss = PostSerializer
+    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
 
 class CommentListViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
 
 
