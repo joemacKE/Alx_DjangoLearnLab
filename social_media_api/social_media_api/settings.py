3df7409546347a1df37afb2335e04ddd6b68d24a
joemacKE <josephmuga2020@gmail.com>
2025-08-21 14:17:35 +0300

    Configured postgresql into my application.

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -100,12 +100,19 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'social_media_database'),
+        'USER': os.getenv('DB_USER', 'social_media_user'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'JosephMuga@2020'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
+
 
 
 # Password validation
